ddcf38af6d3eec7ee0bb7420ff99171ae23bf165
cjvisser <cornevisser0808@gmail.com>
2020-09-26 14:19:22 +0200

    wip

--- assignment1_2/src/main/java/com/cornevisser/assignment1_2/ReversingController.java
+++ assignment1_2/src/main/java/com/cornevisser/assignment1_2/ReversingController.java
@@ -1,2 +1,17 @@
-package com.cornevisser.assignment1_2;public class reversingController {
+package com.cornevisser.assignment1_2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReversingController {
+    @GetMapping("/reverse")
+    public String reverser(String text) {
+        return new StringBuilder(text).reverse().toString();
+    }
+
+    @GetMapping("/counter")
+    public int counter(String text){
+        return text.split(" ").length;
+    }
 }
